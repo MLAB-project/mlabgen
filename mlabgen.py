32ccc823584699460354794c543f2fa6ae0bc9b8
Martin Povišer <martin.povik@gmail.com>
2015-07-09 21:43:56 +0200

    Add PrjInfo parsing.

--- mlabgen.py
+++ mlabgen.py
@@ -1,3 +1,6 @@
+import io
+import re
+
 def descr2dict(descr):
     outdict = {}
 
@@ -9,7 +12,7 @@ def descr2dict(descr):
             else:
                 val += item + " "
         outdict[line.split(" ")[0]] = val
-    
+
     return outdict
 
 def dict_str_replace(replace, replacement, dictionary):
@@ -23,9 +26,18 @@ def dict_str_rm(rm, dictionary):
 
 def dict2prjinfo(dictionary):
     outstr = ""
-    
+
     for key in list(dictionary.keys()):
         outstr += "[" + key + "]\n"
         outstr += dictionary[key] + "\n"
 
     return outstr
+
+PRJINFORE = re.compile('\[(?P<Key>[a-z,A-Z,0-9,\-\_\.]+)\]'
+                       + '\s*(?P<Value>.*?)\s*'
+                       + '(?=\[(?:[a-z,A-Z,0-9,\-\_\.]+)\])', flags=re.S|re.U)
+
+def prjinfo2dict(path):
+    content = io.open(path, encoding='utf-8-sig').read()
+    content = '\n'.join([x for x in content.split('\n') if not x.strip().startswith('//')]).strip()
+    return dict(re.findall(PRJINFORE, content))
